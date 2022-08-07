1f22938a1edb610ea82e4cbbc774f48ab3399c33
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-08-07 18:19:39 -0300

    Cleaning

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,12 +2,11 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   const getEmployeeFile = data.employees.find((name) =>
-    name.firstName === employeeName || name.lastName === employeeName);
+    name.firstName === employeeName || name.lastName === employeeName
+  )
   if (employeeName === undefined) {
-    return {};
+    return {}
   }
-  return getEmployeeFile;
+  return getEmployeeFile
 }
-
-console.log(getEmployeeByName('Nigel'));
 module.exports = getEmployeeByName;

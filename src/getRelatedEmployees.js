00942fb7e4c484156b84e04fd03e3332d863bb4d
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-08-07 20:28:44 -0300

    1 hour trying 2nd part of req 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  // Considerando que o teste espera const managers = [stephanieId, olaId, burlId]
+  //como gerentes:
+  return checkingManagers = data.employees.some((managerCheck) => managerCheck.managers.includes(id));
 }
-
+isManager('9e7d4524-363c-416a-8759-8aa7e50c0992')
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+ const acc = []
+  if (isManager(managerId)){
+    const team = data.employees.managers.forEach((item) => {
+      if(item === managerId){
+        acc.push(item)
+        console.log(acc);
+      }
+      console.log(acc);
+      return acc
+      
+    },)
+    console.log(team);
+    return team;
+    
+  }
+  
 }
 
 module.exports = { isManager, getRelatedEmployees };

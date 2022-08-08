b997b01b3bad0f6e7583ac122cecc1e1d88b0244
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-08-07 22:40:34 -0300

    Req 5 done (Remake after few days)

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const animalsQtd = data.species.reduce((acc, item) => {
+      const animals = item.name;
+      const animalsAmount = item.residents.length;
+      acc[animals] = animalsAmount;
+      return acc;
+    }, {});
+    return animalsQtd;
+  }
+  if (animal.sex) {
+    return data.species.find((e) => e.name === animal.specie).residents
+      .filter((e) => e.sex === animal.sex).length;
+  }
+  if (animal.specie) {
+    const a = data.species.find((e) => e.name === animal.specie).residents.length;
+    return a;
+  }
 }
 
 module.exports = countAnimals;

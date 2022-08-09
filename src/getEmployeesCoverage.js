6beba997700a28e477be414527f8bc29feec698a
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-08-09 00:20:27 -0300

    Somehow func got executed infinite times

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,6 +1,6 @@
 const data = require('../data/zoo_data');
 
-const takeItAll = data.employees.map((e) => (
+const takeItAll = () => data.employees.map((e) => (
   {
     id: e.id,
     fullName: `${e.firstName}${e.lastName}`,
@@ -9,13 +9,24 @@ const takeItAll = data.employees.map((e) => (
   }));
 
 function getEmployeesCoverage(info) {
-  if (info && takeItAll.find((e) => e.id === info.id
+  /* if (info && takeItAll.find((e) => e.id === info.id
 || e.fullName.includes(info.name))) {
     return takeItAll;
+  } */
+  let test;
+  if(!info){
+    return data.employees.map((q) => takeItAll(q))
+  }
+  if (info.name){
+    test = (a) => a.firstName === info.name || a.lastName === info.name;
+  }
+  if(info.id){
+    test = (a) => a.id === info.id
   }
   if (info) {
     throw new Error('Informações inválidas');
   }
+  return takeItAll(e)
 }
 
 module.exports = getEmployeesCoverage;

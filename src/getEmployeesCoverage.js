fef19b0e7e42dac7f71b7a983be55b36d6f76076
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-08-08 23:51:49 -0300

    At least throw Error i got right. Lint fixes as well

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,22 +2,20 @@ const data = require('../data/zoo_data');
 
 const takeItAll = data.employees.map((e) => (
   {
-  id: e.id,
-  fullName:`${e.firstName}${e.lastName}`,
-  species: e.responsibleFor.map((z) => (data.species.find((a) => a.id === z).name)),
-  locations: e.responsibleFor.map((z) => (data.species.find((a) => a.id === z).location))
-}))
+    id: e.id,
+    fullName: `${e.firstName}${e.lastName}`,
+    species: e.responsibleFor.map((z) => (data.species.find((a) => a.id === z).name)),
+    locations: e.responsibleFor.map((z) => (data.species.find((a) => a.id === z).location)),
+  }));
 
 function getEmployeesCoverage(info) {
-if (info && takeItAll.find ((e) => e.id === info.id ||
-e.fullName.includes(info.name))){
-  return takeItAll
+  if (info && takeItAll.find((e) => e.id === info.id
+|| e.fullName.includes(info.name))) {
+    return takeItAll;
+  }
+  if (info) {
+    throw new Error('Informações inválidas');
+  }
 }
-if (info){
-  throw new error ('Informações Inválidas');
-}
-}
-
-
 
 module.exports = getEmployeesCoverage;

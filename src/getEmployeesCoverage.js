1a5b22dc5f3fc00ffeda167c101c541b933d884d
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-08-08 23:48:49 -0300

    Almost giving up

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,15 +1,23 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+const takeItAll = data.employees.map((e) => (
+  {
+  id: e.id,
+  fullName:`${e.firstName}${e.lastName}`,
+  species: e.responsibleFor.map((z) => (data.species.find((a) => a.id === z).name)),
+  locations: e.responsibleFor.map((z) => (data.species.find((a) => a.id === z).location))
+}))
 
+function getEmployeesCoverage(info) {
+if (info && takeItAll.find ((e) => e.id === info.id ||
+e.fullName.includes(info.name))){
+  return takeItAll
+}
+if (info){
+  throw new error ('Informações Inválidas');
+}
 }
 
-function getEmployee(info) {
-  const employee = data.employees.find((e) =>
-    e.id === info.id || e.firstName === info.name || e.lastName === info.name)
-  return employee;
-}
 
-function getEmployeeByName()
 
 module.exports = getEmployeesCoverage;

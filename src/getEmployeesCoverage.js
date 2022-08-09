0386c22eb9f365ef15ed5731228a3de54d0611db
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-08-09 01:26:41 -0300

    Missing ) on 7,8:; Missing space on split method; I cant believe i spent HOURS on this. Must be tired. Rly need to organize my time on project better. ALMOST 15 hours on this project

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -4,14 +4,14 @@ const takeItAll = data.employees.map((e) => (
   {
     id: e.id,
     fullName: `${e.firstName} ${e.lastName}`,
-    species: e.responsibleFor.map((z) => (data.species.find((a) => a.id === z).name)),
-    locations: e.responsibleFor.map((z) => (data.species.find((a) => a.id === z).location)),
+    species: e.responsibleFor.map((z) => (data.species.find((a) => (a.id === z)).name)),
+    locations: e.responsibleFor.map((z) => (data.species.find((a) => (a.id === z)).location)),
   }));
 
 function getEmployeesCoverage(info) {
   const test = info && takeItAll.find((e) =>
-    e.id === info.id || e.fullName.split('')[0] === info.name
-  || e.fullName.split('')[1] === info.fullName);
+    e.id === info.id || e.fullName.split(' ')[0] === info.name
+  || e.fullName.split(' ')[1] === info.name);
   if (test) { return test; }
 
   if (info) {

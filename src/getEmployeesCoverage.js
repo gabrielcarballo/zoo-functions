4991ca0702bc5363a62e9137a9590cce425a559c
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-08-09 01:01:19 -0300

    got a bit of advance

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -3,15 +3,16 @@ const data = require('../data/zoo_data');
 const takeItAll = data.employees.map((e) => (
   {
     id: e.id,
-    fullName: `${e.firstName}${e.lastName}`,
+    fullName: `${e.firstName} ${e.lastName}`,
     species: e.responsibleFor.map((z) => (data.species.find((a) => a.id === z).name)),
     locations: e.responsibleFor.map((z) => (data.species.find((a) => a.id === z).location)),
   }));
 
 function getEmployeesCoverage(info) {
-  if (info && takeItAll.find((e) => e.id === info.id || e.fullName.match(info.name))) {
-    return;
-  }
+  const test = info && takeItAll.find((e) =>
+    e.id === info.id || e.fullName.split('')[0] === info.name
+  || e.fullName.split('')[1] === info.fullName);
+  if (test) { return test; }
 
   if (info) {
     throw new Error('Informações inválidas');
